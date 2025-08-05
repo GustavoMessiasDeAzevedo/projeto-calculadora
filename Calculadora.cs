68c35a8de84431fa9a312d90a22c4f1e55bbbe40
Gustavo Messias <gustavo.mazevedo1@senacsp.edu.br>
2025-08-05 20:53:15 -0300

    Adiciona o programa do botão subtrair

--- Calculadora.cs
+++ Calculadora.cs
@@ -15,6 +15,11 @@ namespace Calculadora
 
         }
 
-    
+        private void btnSubtrair_Click(object sender, EventArgs e)
+        {
+            double numero1 = Convert.ToDouble(txtNumero1.Text);
+            double numero2 = Convert.ToDouble(txtNumero2.Text);
+            lblResultado.Text = (numero1 - numero2).ToString();
+        }
     }
 }
